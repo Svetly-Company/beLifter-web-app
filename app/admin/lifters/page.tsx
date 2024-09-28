068153e6e2c2721f04fb4c531eec958b5330b982
ioolliver <a>
2024-09-28 20:07:32 -0300

    feat: alunos dashboard

--- app/admin/lifters/page.tsx
+++ app/admin/lifters/page.tsx
@@ -2,6 +2,23 @@ import { API } from "@/utils/constants";
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation";
 import { Navbar } from "../navbar";
+import { Search } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
+function Aluno({ user } : { user: any }) {
+    return (
+    <div className="bg-[#232324] m-2 px-8 py-2 flex rounded-2xl">
+        <Avatar className="w-16 h-16 rounded-full">
+            <AvatarImage className="w-16 h-16 rounded-full" src={user.mediaUrl} />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col justify-center m-2">
+            <h1>{user.name}</h1>
+            <h2 className="text-[#979797]">isaque@gmail.com</h2>
+        </div>
+    </div>
+    )
+}
 
 export default async function Page() {
     const cookiesStore = cookies();
@@ -11,6 +28,8 @@ export default async function Page() {
     try{
         const user_req = await API.get("auth/profile", token.value);
         const user = user_req.data;
+        const lifters_req = await API.get("chat", token.value);
+        const lifters = lifters_req.data;
         return (
             <section className="h-screen flex p-8">
                 <Navbar selectedIndex={1} />
@@ -18,6 +37,22 @@ export default async function Page() {
                     <div className="flex justify-around">
                         <h1 className="text-3xl font-semibold p-4 text-center">Gerencie seus alunos aqui, {user.name}.</h1>
                         <div className="bg-white w-16 h-16 rounded-full"></div>
+                    </div>
+                    <div className="bg-[#151415] m-6 p-4">
+                        <div className="flex flex-col">
+                            <div className="flex items-center justify-between">
+                                <h1 className="text-2xl font-medium">Alunos cadastrados <span className="font-bold text-[#aaa]">({lifters.length})</span></h1>
+                                <div className="">
+                                    <div className="flex gap-2 border-2 border-[#0077B6] rounded-2xl items-center px-2 py-1">
+                                        <Search color="#0077B6" />
+                                        <input type="text" placeholder="Procurar Aluno" className="bg-[#151415] m-2 outline-none" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col">
+                                {lifters.map((l : any) => <Aluno user={l} />)}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
